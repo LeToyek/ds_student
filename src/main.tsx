33b7d3f0b3263096f4ee887cc47f94cc60ed12c9
Maulana Arif Wijaya <maulanatoyek@gmail.com>
2024-09-07 23:32:35 +0700

    chore(header): add dynamic heading web

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,25 @@
 import { ThemeProvider } from "@material-tailwind/react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { HelmetProvider } from "react-helmet-async";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
-import { PredictPage } from "./ui/pages/PredictPage.tsx"; // Import your new Predict component
 import KanbanPage from "./ui/pages/KanbanPage.tsx";
+import { PredictPage } from "./ui/pages/PredictPage.tsx"; // Import your new Predict component
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/predict" element={<PredictPage />} />
-          <Route path='/kanban' element={<KanbanPage />} />
-        </Routes>
-      </BrowserRouter>
-    </ThemeProvider>
+    <HelmetProvider>
+      <ThemeProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="/predict" element={<PredictPage />} />
+            <Route path="/kanban" element={<KanbanPage />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
+    </HelmetProvider>
   </StrictMode>
 );
